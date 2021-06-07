a9e0cd6836ec160bbaec767252acd900dc32b538
Daniele <daniele.iuliano1@studenti.unicampania.it>
2021-06-07 16:08:04 +0200

    Esercizi modulo 4 (lampadina n1)

---
+++ Esercizi-Modulo_4/src/Modulo3/Main.java
@@ -0,0 +1,16 @@
+package Modulo3;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Lampadina lamp = new Lampadina();
+        System.out.println(lamp.getStato());
+        lamp.click();
+        lamp.click();
+        lamp.click();
+        lamp.click();
+        lamp.click();
+        lamp.click();
+
+    }
+}
